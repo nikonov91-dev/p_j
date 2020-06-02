70a6e62d575f9e19b1aaf4925310d6958188bcf2
Alex Nikonov <31586394+nikonov91-dev@users.noreply.github.com>
2020-06-02 17:12:54 +0300

    Update devoir1-execute-me.py

--- devoir1-execute-me.py
+++ devoir1-execute-me.py
@@ -1,10 +1,11 @@
 # 1.1
-from api_services.arvix_api import request_and_parse_docs
+from api_services.arvix_api import request_and_parse_docs as arvix
+from api_services.reddit_api import request_and_parse_docs as reddit
 
 #ici Comment ca march est claire.
-reddit_posts = request_and_parse_docs()
+reddit_posts = reddit()
 
-arvix_posts = request_and_parse_docs()
+arvix_posts = arvix()
 
 print('reddit_posts')
 print(reddit_posts)
